af2b54b146136b932bb37ce02ec2f6ec0c4d4803
Kab-35-16 <Kab-35-16@KAB-31-3->
2024-10-22 10:47:20 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    Console.WriteLine("a=");
+            //    int a = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("b=");
+            //    int b = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("c=");
+            //    int c = int.Parse(Console.ReadLine());
+            //    double d = Math.Pow(b, 2) - 4 * a * c;
+            //    if (d > 0)
+            //    {
+            //        Console.WriteLine("2 КОРЕНЬ");
+            //        double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+            //        double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+            //        Console.WriteLine($"X1={x1:f2}\tx2={x2:f2}");
+            //    }
+            //    if (d == 0)
+            //    {
+            //        Console.WriteLine(" КОРЕНЬ");
+            //        double x = -d / (2 * a);
+            //        Console.WriteLine("x=" + x);
+            //    }
+            //    if (d < 0)
+            //    {
+            //        Console.WriteLine("НЕТ КОРНЕЙ");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"Исключение: {ex.Message}");
+            //    Console.WriteLine($"Метод: {ex.TargetSite}");
+            //    Console.WriteLine($"Трассировка стека: {ex.StackTrace}");
+            //}
+            //Console.Read();
+
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //intConsole.WriteLine("Введите стороны треугольника a, b, c:");
+            // b = Convert.ToInt32(Console.ReadLine());
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //if (a + b > c && a + c > b && b + c > a) // проверяем существование треугольника
+            //{
+            //    double p = (a + b + c) / 2.0; // полупериметр
+            //    double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c)); // формула Герона для вычисления площади
+            //    Console.WriteLine($"Площадь треугольника: {area}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Треугольник не существует");
+            //}
+            //Console.Read();
+        }
+    }
+}
